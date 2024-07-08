f0a7cf031f038089a1abe3ec427f5c508dd5885a
Erik Grip <erik.grip@outlook.com>
2024-07-08 16:03:52 +0200

    Fix mocking data file in test.

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -10,11 +10,11 @@ from src.data.dataset import MovieLensDataset
 def fixture_mock_open():
     """Mock the zipfile.ZipFile.open to use the test fixture file."""
     mock_data_path = "tests/fixtures/ratings.csv"
-    with patch("src.data.dataset.zipfile.ZipFile.__enter__") as mock:
-        mock.return_value = mock
-        mock.open = MagicMock()
-        mock.open.return_value.__enter__.return_value = open(mock_data_path, "rb")
-        yield mock
+    mock_zipfile = MagicMock()
+    mock_zipfile.__enter__.return_value = mock_zipfile
+    mock_zipfile.open = MagicMock(return_value=open(mock_data_path, "rb"))
+    with patch("zipfile.ZipFile", MagicMock(return_value=mock_zipfile)):
+        yield mock_zipfile
 
 
 def test_movie_lens_dataset(mock_data_file):  # pylint: disable=unused-argument
