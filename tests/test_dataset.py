d326c010c699c8a3bc052f625c34ab3e0bc8592c
Erik Grip <erik.grip@outlook.com>
2024-07-08 15:07:20 +0200

    Fix mocking data file.

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -6,17 +6,18 @@ import torch
 from src.data.dataset import MovieLensDataset
 
 
-@pytest.fixture(name="mock_open")
+@pytest.fixture(name="mock_data_file")
 def fixture_mock_open():
     """Mock the zipfile.ZipFile.open to use the test fixture file."""
     mock_data_path = "tests/fixtures/ratings.csv"
-    with patch("src.data.dataset.zipfile.ZipFile.open") as mock_open:
-        mock_open.return_value = MagicMock()
-        mock_open.return_value.__enter__.return_value = open(mock_data_path, "rb")
-        yield mock_open
+    with patch("src.data.dataset.zipfile.ZipFile.__enter__") as mock:
+        mock.return_value = mock
+        mock.open = MagicMock()
+        mock.open.return_value.__enter__.return_value = open(mock_data_path, "rb")
+        yield mock
 
 
-def test_movie_lens_dataset(mock_open):  # pylint: disable=unused-argument
+def test_movie_lens_dataset(mock_data_file):  # pylint: disable=unused-argument
     """Test the MovieLensDataset class."""
     dataset = MovieLensDataset()
     assert len(dataset) == 3
