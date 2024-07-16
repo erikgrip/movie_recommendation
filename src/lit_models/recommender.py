f2bc456f192ed7e4f71cb11f60a9e9413342dae9
Erik Grip <erik.grip@outlook.com>
2024-07-16 22:46:55 +0200

    wip: implement training_step.

--- src/lit_models/recommender.py
+++ src/lit_models/recommender.py
@@ -1,7 +1,11 @@
 """ Movie recommendation model. """
 
+# pylint: disable=arguments-differ,unused-argument
+
 import pytorch_lightning as pl
 import torch
+
+from src.models.embedding_model import RecommendationModel
 
 
 class LitRecommender(pl.LightningModule):
@@ -9,15 +13,31 @@ class LitRecommender(pl.LightningModule):
 
     def __init__(self):
         super().__init__()
+        self.model = RecommendationModel(
+            num_users=1000, num_movies=1000, embedding_size=256, hidden_dim=256, dropout_rate=0.2
+        )
 
     def forward(self, *args, x=None, **kwargs):
         """Forward pass of the model."""
         # in lightning, forward defines the prediction/inference actions
 
-    def training_step(self, *args, batch=None, batch_idx=None, **kwargs):
+    def training_step(self, train_batch=None, batch_idx=None):
         """Training step."""
-        # training_step defines the train loop. It is independent of forward
-        # return loss
+        output = recommendation_model(
+            train_data["users"].to(device), train_data["movies"].to(device)
+        )
+        # Reshape the model output to match the target's shape
+        output = output.squeeze()  # Removes the singleton dimension
+        ratings = (
+            train_data["ratings"].to(torch.float32).to(device)
+        )  # Assuming ratings is already 1D
+
+        loss = loss_func(output, ratings)
+        inputs, target = train_batch
+        output = self.model(inputs, target)
+        loss = torch.nn.MSELoss(output, target.view(-1))
+        self.log("train_loss", loss)
+        return loss
 
     def configure_optimizers(self):
         """Configure optimizers and learning rate schedulers."""
