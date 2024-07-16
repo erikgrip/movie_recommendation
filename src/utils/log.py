06163683705f7844513499c979fab08f456f31ea
Erik Grip <erik.grip@outlook.com>
2024-07-16 09:16:03 +0200

    Add logger util.

---
+++ src/utils/log.py
@@ -0,0 +1,8 @@
+""" Logging configuration for the project. """
+
+import logging
+
+logging.getLogger("torch").setLevel(logging.WARNING)
+
+logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.INFO)
+logger = logging.getLogger(__name__)
