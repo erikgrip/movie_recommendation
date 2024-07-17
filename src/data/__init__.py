b4a8b2df693c55701a31957b71365a918f6f88f1
Erik Grip <erik.grip@outlook.com>
2024-07-18 00:00:19 +0200

    Add imports to init files for convenience.

---
+++ src/data/__init__.py
@@ -0,0 +1,5 @@
+""" This module contains all the datasets and data modules for the recommender system. """
+
+# pylint: disable=unused-import
+
+from src.data.data_module import MovieLensDataModule
---
+++ src/models/__init__.py
@@ -0,0 +1,5 @@
+""" This module contains all the models for the recommender system. """
+
+# pylint: disable=unused-import
+
+from src.models.embedding_model import RecommendationModel
