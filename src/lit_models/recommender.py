36574e49614fe7b3f96ea0ec57d58d021f30c94d
Erik Grip <erik.grip@outlook.com>
2024-07-18 15:45:34 +0200

    Add argparse args to lit_model recommender.

--- src/lit_models/recommender.py
+++ src/lit_models/recommender.py
@@ -10,15 +10,45 @@ import torch.nn.functional as F
 
 from src.utils.log import logger
 
+LR = 1e-3
+OPTIMIZER = "Adam"
+ONE_CYCLE_TOTAL_STEPS = 100
+
 
 class LitRecommender(pl.LightningModule):
     """PyTorch Lightning module for the movie recommendation model."""
 
     def __init__(self, model: torch.nn.Module, args: Optional[Dict] = None):
         super().__init__()
-        self.args = args or {}
         self.model = model
+        self.args = args or {}
+        optimizer = self.args.get("optimizer", OPTIMIZER)
+        self.optimizer_class = getattr(torch.optim, optimizer)
+        self.lr = self.args.get("lr", LR)
+        self.one_cycle_max_lr = self.args.get("one_cycle_max_lr")
+        self.one_cycle_total_steps = self.args.get(
+            "one_cycle_total_steps", ONE_CYCLE_TOTAL_STEPS
+        )
         self.training_step_losses: list[torch.Tensor] = []
+
+    @staticmethod
+    def add_to_argparse(parser):  # pylint: disable=missing-function-docstring
+        parser.add_argument(
+            "--optimizer",
+            type=str,
+            default=OPTIMIZER,
+            help=f"Optimizer class from torch.optim (default: {OPTIMIZER})",
+        )
+        parser.add_argument(
+            "--lr", type=float, default=LR, help=f"learning rate (default: {LR})"
+        )
+        parser.add_argument(
+            "--one_cycle_total_steps",
+            type=int,
+            default=ONE_CYCLE_TOTAL_STEPS,
+            help="Total steps for the 1cycle policy",
+        )
+        return parser
 
     def forward(self, *args, x=None, **kwargs):
         """Forward pass of the model."""
@@ -46,7 +76,14 @@ class LitRecommender(pl.LightningModule):
         )
         self.training_step_losses.clear()  # free memory
 
-    def configure_optimizers(self) -> torch.optim.Optimizer:
-        """Configure optimizers and learning rate schedulers."""
-        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
-        return optimizer
+    def configure_optimizers(self):
+        """Initialize optimizer and learning rate scheduler."""
+        optimizer = self.optimizer_class(self.parameters(), lr=self.lr)
+        if self.one_cycle_max_lr is None:
+            return {"optimizer": optimizer}
+        scheduler = torch.optim.lr_scheduler.OneCycleLR(
+            optimizer=optimizer,
+            max_lr=self.one_cycle_max_lr,
+            total_steps=self.one_cycle_total_steps,
+        )
+        return {"optimizer": optimizer, "lr_scheduler": scheduler}
--- tests/test_recommender.py
+++ tests/test_recommender.py
@@ -9,8 +9,8 @@ from tests.mocking import (  # pylint: disable=unused-import
     fixture_data_module, fixture_mock_zip)
 
 
-@pytest.fixture(name="lit_model")
-def fixture_lit_model(data_module):
+@pytest.fixture(name="model")
+def fixture_model(data_module):
     """Create an example LitRecommender model."""
     data_module.prepare_data()
     data_module.setup()
@@ -20,12 +20,27 @@ def fixture_lit_model(data_module):
         "hidden_dim": 4,
         "dropout_rate": 0.0,
     }
-    model = RecommendationModel(
+    return RecommendationModel(
         num_users=data_module.num_user_labels(),
         num_movies=data_module.num_movie_labels(),
         args=args,
     )
-    return LitRecommender(model=model)
+
+
+@pytest.fixture(name="lit_model")
+def fixture_lit_model(model):
+    """Create an example LitRecommender model."""
+    return LitRecommender(model)
+
+
+def test_lit_recommender_init(model):
+    """Test the initialization of LitRecommender."""
+    lit_model = LitRecommender(model=model)
+    assert lit_model.optimizer_class == torch.optim.Adam
+    assert lit_model.lr == 1e-3
+    assert lit_model.one_cycle_max_lr is None
+    assert lit_model.one_cycle_total_steps == 100
+    assert not lit_model.training_step_losses
 
 
 def test_lit_recommender_forward(lit_model):
@@ -51,5 +66,38 @@ def test_lit_recommender_training_step(lit_model):
 
 def test_lit_recommender_configure_optimizers(lit_model):
     """Test the configure_optimizers method of LitRecommender."""
-    optimizer = lit_model.configure_optimizers()
-    assert isinstance(optimizer, torch.optim.Optimizer)
+    cfg = lit_model.configure_optimizers()
+    assert isinstance(cfg["optimizer"], torch.optim.Optimizer)
+    assert cfg.get("lr_scheduler") is None
+
+
+@pytest.mark.parametrize(
+    "args,expected_optimizer,expected_lr_scheduler,expected_lr",
+    [
+        ({"optimizer": "Adam"}, torch.optim.Adam, None, 1e-3),
+        ({"optimizer": "SGD"}, torch.optim.SGD, None, 1e-3),
+        ({"optimizer": "AdamW"}, torch.optim.AdamW, None, 1e-3),
+        ({"lr": 0.1}, torch.optim.Adam, None, 0.1),
+        (
+            {"one_cycle_max_lr": 0.1},
+            torch.optim.Adam,
+            torch.optim.lr_scheduler.OneCycleLR,
+            1e-3,
+        ),
+        ({"one_cycle_total_steps": 100}, torch.optim.Adam, None, 1e-3),
+    ],
+)
+def test_lit_recommender_configure_optimizers_non_default(
+    model, args, expected_optimizer, expected_lr_scheduler, expected_lr
+):
+    """Test the configure_optimizers method of LitRecommender."""
+    lit_model = LitRecommender(model=model, args=args)
+    cfg = lit_model.configure_optimizers()
+    optimizer = cfg["optimizer"]
+    lr_scheduler = cfg.get("lr_scheduler")
+    assert isinstance(optimizer, expected_optimizer)
+    assert optimizer.defaults["lr"] == expected_lr
+    if expected_lr_scheduler:
+        assert isinstance(lr_scheduler, expected_lr_scheduler)
+    else:
+        assert lr_scheduler is None
