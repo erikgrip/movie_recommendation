fdbc34042d138a8ebc4062fbbb85b70c5eb991f1
Erik Grip <erik.grip@outlook.com>
2024-12-27 10:08:15 +0100

    Format

--- src/lit_models/lit_two_tower.py
+++ src/lit_models/lit_two_tower.py
@@ -1,9 +1,11 @@
+# pylint: disable=missing-function-docstring
 """ PyTorch Lightning module for the Two-Tower training loop."""
 
 from argparse import ArgumentParser
 from typing import Dict, Optional
 
 import torch
+from torch import Tensor
 from torchmetrics import MeanSquaredError, Metric
 
 from src.lit_models.base_model import BaseLitModel
@@ -24,42 +26,35 @@ class TwoTowerLitModel(BaseLitModel):  # pylint: disable=too-many-ancestors
         """Add model-specific arguments to the parser."""
         return parser
 
-    def forward(self, x: Optional[Dict[str, torch.Tensor]] = None) -> torch.Tensor:
+    def forward(self, x: Optional[Dict[str, Tensor]] = None) -> Tensor:
         """Forward pass of the model."""
         # TODO: Concatenate earlier so that we can just pass the input on here
         if x is None:
             raise ValueError("batch argument is required for TwoTowerLitModel.")
         return self.model(**{k: v for k, v in x.items() if k != "labels"})
 
-    def training_step(
-        self, batch: Dict[str, torch.Tensor], batch_idx: int
-    ) -> torch.Tensor:
-        """Training step."""
+    def _get_loss(self, batch: Dict[str, Tensor]) -> Tensor:
+        """Compute the loss for a batch."""
         denorm_preds = self(batch) * MAX_RATING
-        loss = self.mse(denorm_preds, batch["labels"])
+        return self.mse(denorm_preds, batch["labels"])
+
+    def training_step(self, batch: Dict[str, Tensor], batch_idx: int) -> Tensor:
+        loss = self._get_loss(batch)
         self.log("train_loss", loss, on_step=True, on_epoch=True, prog_bar=True)
         return loss
 
-    def validation_step(
-        self, batch: Dict[str, torch.Tensor], batch_idx: int
-    ) -> torch.Tensor:
-        """Validation step."""
-        denorm_preds = self(batch) * MAX_RATING
-        loss = self.mse(denorm_preds, batch["labels"])
+    # pylint: disable=code-duplication
+    def validation_step(self, batch: Dict[str, Tensor], batch_idx: int) -> Tensor:
+        loss = self._get_loss(batch)
         self.log("val_loss", loss, on_step=False, on_epoch=True, prog_bar=True)
         return loss
 
-    def test_step(
-        self, batch: Dict[str, torch.Tensor], batch_idx: int
-    ) -> Dict[str, torch.Tensor]:
-        """Test step."""
-        denorm_preds = self(batch) * MAX_RATING
-        loss = self.mse(denorm_preds, batch["labels"])
+    def test_step(self, batch: Dict[str, Tensor], batch_idx: int) -> Dict[str, Tensor]:
+        loss = self._get_loss(batch)
         self.log("test_loss", loss, on_step=False, on_epoch=True, prog_bar=True)
         return {"loss": loss}
 
     def predict_step(
-        self, batch: Dict[str, torch.Tensor], batch_idx: int, dataloader_idx: int = 0
-    ) -> torch.Tensor:
-        """Prediction step."""
+        self, batch: Dict[str, Tensor], batch_idx: int, dataloader_idx: int = 0
+    ) -> Tensor:
         return self.forward(batch)
