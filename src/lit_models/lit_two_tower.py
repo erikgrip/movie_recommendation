5f93cade8b9b6d77cca8ab201a455d5944c900a1
Erik Grip <erik.grip@outlook.com>
2024-12-27 11:15:42 +0100

    Cleanup.

--- src/lit_models/lit_two_tower.py
+++ src/lit_models/lit_two_tower.py
@@ -26,30 +26,27 @@ class TwoTowerLitModel(BaseLitModel):  # pylint: disable=too-many-ancestors
         """Add model-specific arguments to the parser."""
         return parser
 
-    def forward(self, x: Optional[Dict[str, Tensor]] = None) -> Tensor:
+    def forward(self, x: Dict[str, Tensor]) -> Tensor:
         """Forward pass of the model."""
-        # TODO: Concatenate earlier so that we can just pass the input on here
-        if x is None:
-            raise ValueError("batch argument is required for TwoTowerLitModel.")
         return self.model(**{k: v for k, v in x.items() if k != "target"})
 
-    def _get_loss(self, batch: Dict[str, Tensor]) -> Tensor:
+    def _batch_loss(self, batch: Dict[str, Tensor]) -> Tensor:
         """Compute the loss for a batch."""
         denorm_preds = self(batch) * MAX_RATING
         return self.mse(denorm_preds, batch["target"])
 
     def training_step(self, batch: Dict[str, Tensor], batch_idx: int) -> Tensor:
-        loss = self._get_loss(batch)
+        loss = self._batch_loss(batch)
         self.log("train_loss", loss, on_step=True, on_epoch=True, prog_bar=True)
         return loss
 
     def validation_step(self, batch: Dict[str, Tensor], batch_idx: int) -> Tensor:
-        loss = self._get_loss(batch)
+        loss = self._batch_loss(batch)
         self.log("val_loss", loss, on_step=False, on_epoch=True, prog_bar=True)
         return loss
 
     def test_step(self, batch: Dict[str, Tensor], batch_idx: int) -> Dict[str, Tensor]:
-        loss = self._get_loss(batch)
+        loss = self._batch_loss(batch)
         self.log("test_loss", loss, on_step=False, on_epoch=True, prog_bar=True)
         return {"loss": loss}
 
