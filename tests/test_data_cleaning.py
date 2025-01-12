e249698a28309e9e64848f074593924532edf513
Erik Grip <erik.grip@outlook.com>
2025-01-12 21:00:17 +0100

    Move data cleaning tests to separate file.

---
+++ tests/test_data_cleaning.py
@@ -0,0 +1,51 @@
+# pylint: disable=missing-function-docstring
+""" Tests for the data cleaning module """
+
+import numpy as np
+import pandas as pd
+
+from prepare_data.clean import (
+    clean_movie_titles,
+    extract_movie_release_year,
+    impute_missing_year,
+)
+
+# TODO: Create main funtion for data cleaning and test it
+
+
+def test_extract_movie_release_year():
+    titles = pd.Series(["Toy Story (1995)", "Jumanji (1995)", "No Year"])
+
+    expected_output = pd.Series([1995.0, 1995.0, np.nan], name=0)
+    output = extract_movie_release_year(titles)
+
+    pd.testing.assert_series_equal(output, expected_output)
+
+
+def test_impute_missing_year():
+    movies = pd.DataFrame({"movie_id": [1, 2, 3], "year": [1950.0, np.nan, np.nan]})
+
+    ratings = pd.DataFrame(
+        {
+            "movie_id": [1, 2, 3],
+            "timestamp": pd.to_datetime(
+                ["1951-01-01 10:00:00", "1999-01-20 13:00:00", "2021-02-05 15:00:00"]
+            ),
+        }
+    )
+
+    expected_output = pd.DataFrame(
+        {"movie_id": [1, 2, 3], "year": [1950, 1999, 2021]}, dtype=int
+    )
+    output = impute_missing_year(movies, ratings)
+
+    pd.testing.assert_frame_equal(output, expected_output)
+
+
+def test_clean_movie_titles():
+    titles = pd.Series(["Toy Story (1995)", "Jumanji (1995)", "No Year"])
+
+    expected_output = pd.Series(["Toy Story", "Jumanji", "No Year"])
+    output = clean_movie_titles(titles)
+
+    pd.testing.assert_series_equal(output, expected_output)
---
+++ tests/test_feature_calcualtion.py
@@ -0,0 +1,126 @@
+# pylint: disable=missing-function-docstring
+""" Tests for the features module """
+
+from unittest.mock import patch
+
+import numpy as np
+import pandas as pd
+import pytest
+
+from prepare_data.features import (
+    genre_dummies,
+    text_embedding,
+    user_genre_avg_ratings,
+)
+
+
+def test_genre_dummies():
+    movies = pd.DataFrame({"movie_id": [1, 2], "genres": ["Action", "Action|Thriller"]})
+
+    expected_output = pd.DataFrame(
+        {"movie_id": [1, 2], "is_action": [1, 1], "is_thriller": [0, 1]}
+    )
+    output = genre_dummies(movies)
+
+    pd.testing.assert_frame_equal(output, expected_output)
+
+
+def test_genre_dummies_filter():
+    movies = pd.DataFrame(
+        {"movie_id": [1, 2], "genres": ["Action", "(no genres listed)"]}
+    )
+
+    expected_output = pd.DataFrame({"movie_id": [1, 2], "is_action": [1, 0]})
+    output = genre_dummies(movies)
+
+    pd.testing.assert_frame_equal(output, expected_output)
+
+
+def test_genre_dummies_rename():
+    movies = pd.DataFrame({"movie_id": [1], "genres": ["Action|Sci-Fi"]})
+
+    expected_output = pd.DataFrame({"movie_id": [1], "is_action": [1], "is_sci_fi": [1]})
+    output = genre_dummies(movies)
+
+    pd.testing.assert_frame_equal(output, expected_output)
+
+
+@pytest.mark.parametrize(
+    "dim,expected_output",
+    [
+        (1, [[0.1], [0.4], [0.7]]),
+        (2, [[0.1, 0.2], [0.4, 0.5], [0.7, 0.8]]),
+        (3, [[0.1, 0.2, 0.3], [0.4, 0.5, 0.6], [0.7, 0.8, 0.9]]),
+        (4, [[0.1, 0.2, 0.3], [0.4, 0.5, 0.6], [0.7, 0.8, 0.9]]),
+    ],
+)
+def test_text_embedding(dim, expected_output):
+    titles = ["title1", "title2", "title3"]
+
+    with patch("prepare_data.features.SentenceTransformer") as mock_st:
+        mock_st.return_value.encode.return_value = np.array(
+            [
+                np.array([0.1, 0.2, 0.3]),
+                np.array([0.4, 0.5, 0.6]),
+                np.array([0.7, 0.8, 0.9]),
+            ]
+        )
+
+        output = text_embedding(titles, dim=dim)
+
+    np.testing.assert_array_equal(output, expected_output)
+
+
+def test_user_genre_avg_ratings():
+    ratings = pd.DataFrame(
+        {
+            "user_id": [1, 1, 1, 1, 2, 2],
+            "movie_id": [1, 2, 3, 4, 3, 1],
+            "rating": [5, 4, 3, 2, 4, 4],
+            "timestamp": pd.to_datetime(
+                [
+                    "2021-01-01 10:00:00",
+                    "2021-01-20 13:00:00",
+                    "2021-02-05 15:00:00",
+                    "2021-02-10 09:00:00",
+                    "2021-01-05 10:00:00",
+                    "2021-03-02 10:00:00",
+                ]
+            ),
+        }
+    )
+
+    movie_genre_dummies = pd.DataFrame(
+        [
+            {"movie_id": 1, "action": 1, "comedy": 0},
+            {"movie_id": 2, "action": 0, "comedy": 1},
+            {"movie_id": 3, "action": 1, "comedy": 0},
+            {"movie_id": 4, "action": 0, "comedy": 0},
+        ]
+    )
+
+    expected_output = pd.DataFrame(
+        {
+            "user_id": [1, 1, 1, 1, 2, 2],
+            "timestamp": pd.to_datetime(
+                [
+                    "2021-01-01 10:00:00",
+                    "2021-01-20 13:00:00",
+                    "2021-02-05 15:00:00",
+                    "2021-02-10 09:00:00",
+                    "2021-01-05 10:00:00",
+                    "2021-03-02 10:00:00",
+                ]
+            ),
+            "avg_rating_action": [3.0, 5.0, 5.0, 4.0, 3.0, 4.0],
+            "avg_rating_comedy": [3.0, 3.0, 4.0, 4.0, 3.0, 3.0],
+            "avg_rating_some_other_genre": [3.0, 3.0, 3.0, 3.0, 3.0, 3.0],  # Not rated
+        }
+    )
+
+    with patch(
+        "prepare_data.features.GENRES", ["action", "comedy", "some_other_genre"]
+    ):
+        output = user_genre_avg_ratings(ratings, movie_genre_dummies)
+
+    pd.testing.assert_frame_equal(output, expected_output)
